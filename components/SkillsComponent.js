140299e5341d7853416b9f23f26d03f53826d040
robinrj6 <robinrj6@gmail.com>
2023-06-14 23:56:49 +0530

    SkillsComponent Done

--- components/SkillsComponent.js
+++ components/SkillsComponent.js
@@ -1,9 +1,21 @@
 
-import { useEffect } from 'react';
+import { useEffect, React } from 'react';
 import gsap from 'gsap';
-
+import TagCloud from '@frank-mayer/react-tag-cloud';
+import "../styles/skills.css";
 
 export default function SkillsComponent({ skillsRef }) {
+
+    function getRadius(innerWidth, innerHeight) {
+        if (innerWidth <= 600) {
+            return Math.min(300, innerWidth, innerHeight) / 2;
+        } else if (innerWidth <= 1200) {
+            return Math.min(500, innerWidth, innerHeight) / 2;
+        } else {
+            return Math.min(800, innerWidth, innerHeight) / 2;
+        }
+    }
+
     const animateProfile = () => {
         gsap.from(skillsRef.current, { backgroundColor: "white", duration: 1, delay: .2 }),
             gsap.from(skillsRef.current, { color: "black", duratrion: .5 });
@@ -33,8 +45,40 @@ export default function SkillsComponent({ skillsRef }) {
 
     return (
         <div className="skills section" ref={skillsRef}>
-            <div>
-                Skills Component
+            <div className='centeredText'>
+                <TagCloud
+                    options={(w) => ({
+                        radius: getRadius(w.innerWidth, w.innerHeight),
+                        maxSpeed: "fast",
+                    })}
+                    onClick={(tag, ev) => alert(tag)}
+                    onClickOptions={{ passive: true }}
+                >
+                    {[
+                        "VSCode",
+                        "C/C++",
+                        "React",
+                        "Python",
+                        "Java",
+                        "Springboot",
+                        "NextJs",
+                        "HTML",
+                        "CSS",
+                        "Javascript",
+                        "Three.js",
+                        "Web Development",
+                        "PHP",
+                        "Rest API",
+                        "NodeJs",
+                        "MySQL",
+                        "PostgreSQL",
+                        "OracleDB",
+                        "MongoDB",
+                        "Angular",
+                        "Express",
+                        "Git",
+                    ]}
+                </TagCloud>
             </div>
         </div>
     )
