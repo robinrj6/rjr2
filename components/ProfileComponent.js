f1b128ab4c9a11c4200c9ecef433b2f4e1b94b35
robinrj6 <robinrj6@gmail.com>
2023-09-03 21:50:35 +0530

    Profile text updated

--- components/ProfileComponent.js
+++ components/ProfileComponent.js
@@ -37,7 +37,7 @@ export default function ProfileComponent({ profileRef }) {
                 <img src="/img/Myproject1.jpg"
                     alt="Image"
                     className="imageMe"/>
-                <p className="build"><span className="red">L</span>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="build"><span className="red">M</span>y abiding passion for technology has driven my academic path in computer science and engineering. Alongside a solid academic foundation, I've actively participated in significant projects, including COVID-19 monitoring apps and groundbreaking IoT solutions. My deep learning project on Pneumonia Detection resulted in a published IEEE paper. Proficient in full-stack development, I am enthusiastic about applying my expertise in emerging technologies to contribute to innovative projects.</p>
             </div>
         </div>
     )
