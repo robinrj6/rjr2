7635c881495223c0cfb66db13339af9c60c4a569
SyS <sreyassreelal@gmail.com>
2023-11-21 20:07:56 +0530

    changes to project structure, use page router

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import 'aos/dist/aos.css';
+import '../styles/globals.css'
+import "../styles/fonts.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <Component {...pageProps} />
+    )
+}
